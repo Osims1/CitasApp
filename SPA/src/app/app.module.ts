59fa79a5ebdfb1cb82f3f94bde82adcf4161c055
Osims1 <111721819+Osims1@users.noreply.github.com>
2023-11-17 19:33:30 -0600

    09-LookNFeel

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
+    MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
     TestErrorComponent,
--- SPA/src/app/members/member-detail/member-detail.component.ts
+++ SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,23 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { TabsModule } from 'ngx-bootstrap/tabs';
 import { IMember } from 'src/app/_models/imember';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
-  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css'],
-  imports: [CommonModule, TabsModule, GalleryModule]
+  styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
   member: IMember | undefined;
-  images: GalleryItem[] = [];
-
-  constructor(private membersService: MembersService, private route: ActivatedRoute) { }
+  
+  constructor(private membersService: MembersService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.loadMember();
@@ -27,19 +21,8 @@ export class MemberDetailComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get("username");
     if (!username) return;
     this.membersService.getMember(username).subscribe({
-      next: member => {
-        this.member = member,
-          this.getImages()
-      }
+      next: member => this.member = member
     });
-  }
-
-  getImages() {
-    if (!this.member) return;
-    for (const photo of this.member?.photos) {
-      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
-      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
-    }
   }
 
 }
